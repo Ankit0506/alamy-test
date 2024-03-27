962ef69aa7d55bed94ad7cabb230ebd6f67fdd72
Ankit0506 <30600980+Ankit0506@users.noreply.github.com>
2024-03-27 12:19:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,23 @@
 provider "aws" {
-  region = "us-west-2"
+  region = var.region
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "subnet1" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
+  vpc_id           = aws_vpc.my_vpc.id
+  cidr_block       = var.subnet1_cidr_block
+  availability_zone = "${var.region}a"
 }
 
 resource "aws_subnet" "subnet2" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
-}
-
-provider "aws" {
-  region = "us-east-1"
+  vpc_id           = aws_vpc.my_vpc.id
+  cidr_block       = var.subnet2_cidr_block
+  availability_zone = "${var.region}b"
 }
 
 resource "aws_security_group" "my_security_group" {
@@ -52,9 +48,9 @@ resource "aws_security_group" "my_security_group" {
 
 resource "aws_instance" "my_instance" {
   count         = 2
-  ami           = "ami-12345678"  # Replace with your AMI ID
+  ami           = var.ami
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.my_subnet.id
-  key_name      = "my-key-pair"   # Replace with your key pair name
+  subnet_id     = aws_subnet.subnet1.id
+  key_name      = var.key_name
   security_group_ids = [aws_security_group.my_security_group.id]
 }
